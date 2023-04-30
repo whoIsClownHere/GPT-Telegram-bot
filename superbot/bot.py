f2b61ee87e74969a9e66b3e18c4e85f872dfa930
whoIsClownHere <106624742+whoIsClownHere@users.noreply.github.com>
2023-04-30 19:05:47 +0300

    Update bot.py

--- superbot/bot.py
+++ superbot/bot.py
@@ -1,9 +1,7 @@
-
 import logging
 from telegram.ext import Application, MessageHandler, filters, CommandHandler
 from config import BOT_TOKEN
 from telegram import ReplyKeyboardMarkup
-from translation import trns
 from morphy import morph
 from tts import Tts
 
@@ -13,8 +11,8 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
-reply_keyboard = [['/translate en-ru', '/donation'],
-                      ['/morphology', '/tts']]
+reply_keyboard = [['/help', '/text_to_speach'],
+                      ['/find_information_about_word']]
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False, resize_keyboard=True)
 current_func = 'dialog'
 lang = 'en-ru'
@@ -25,15 +23,6 @@ async def start(update, context):
         "Чем могу помочь?",
         reply_markup=markup
     )
-
-
-async def translation_downloader(update, context):
-    global current_func
-    file = await context.bot.get_file(update.message.document)
-    await file.download_to_drive('data/trans.txt')
-    await update.message.reply_text('С какого языка перевести? (в формате en-ru)')
-    current_func = 'translation-1'
-
 
 def echo(update, context):
     return update.message.text
@@ -51,13 +40,6 @@ async def tts(update, context):
         "Впиши сначала язык, а после запрос.\n Образец - 'ru, Привет!'\n"
         "Доступные языки:\nzh-TW\tКитайский\nen\tАнглийский\nru\tРусский"
         "\nfr\tФранцузский\nes\tИспанский\npt\tПортугальский\nuk\tУкраинский")
-
-
-async def tr_file_or_text(update, context):
-    global current_func, lang
-    lang = context.args[0]
-    await update.effective_message.reply_text('Введите то, что надо перевести (или пришлите файл)')
-    current_func = 'text_translater'
 
 
 async def morphology(update, context):
@@ -83,18 +65,6 @@ async def dialog(update, context):
         print('mp:', update.message.text)
         await update.message.reply_text(morph(update.message.text))
         current_func = 'dialog'
-    elif current_func == 'text_translater':
-        print("tr:", update.message.text)
-        await update.message.reply_text(trns(update.message.text, lang))
-        current_func = 'dialog'
-    elif current_func == 'translation-1':
-        chat_id = update.effective_message.chat_id
-        lang = update.message.text
-        await update.message.reply_text('подождите немного, переводится')
-        file = trns(lang, file=True)
-        with open(file, 'r') as f:
-            await context.bot.send_document(chat_id=chat_id, document=f)
-        current_func = 'dialog'
 
 
 def main():
@@ -103,9 +73,7 @@ def main():
     application.add_handler(CommandHandler("find_information_about_word", morphology))
     application.add_handler(CommandHandler("text_to_speach", tts))
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("text_translater", tr_file_or_text))
     text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, dialog)
-    application.add_handler(MessageHandler(filters.Document.ALL, translation_downloader))
     application.add_handler(text_handler)
     application.run_polling()
 
